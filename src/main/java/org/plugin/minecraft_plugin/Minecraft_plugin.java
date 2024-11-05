82bf982682f014ac46d11a29b1b8caa6f32218e9
GA3RlEL <kruszkagabrys@gmail.com>
2024-11-05 08:26:11 +0100

    SetHome funcionalities added

--- src/main/java/org/plugin/minecraft_plugin/Minecraft_plugin.java
+++ src/main/java/org/plugin/minecraft_plugin/Minecraft_plugin.java
@@ -2,12 +2,15 @@ package org.plugin.minecraft_plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Minecraft_plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("sethome").setExecutor(new SetHome());
+        this.getCommand("home").setExecutor(new Home());
     }
 
     @Override
